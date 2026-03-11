8df809b52280e33fcb8702622600e23c1046d226
Wang yunjie <wangyunjie360@gmail.com>
2026-03-11 11:22:48 +0800

    add

--- scripts/run_robotwin_video_ft.sh
+++ scripts/run_robotwin_video_ft.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -euo pipefail
-
+export PYTHONPATH=${PYTHONPATH:-}:/home/jwhe/linyihan/lingbot-va    
 # 1) Extract raw-video latents into <each_dataset>/latents_video_ft (4 video frames -> 1 latent frame)
 # --dataset_path supports either:
 #   a) a single dataset root (.../meta/info.json)
 #   b) a parent directory containing multiple dataset roots
-# python scripts/robotwin_data_processing/extract_robotwin_video_latents.py \
-#   --dataset_path /home/jwhe/linyihan/datasets/lerobot_robotwin_eef_clean_50 \
-#   --vae_dir /home/jwhe/linyihan/lingbot-va-base/vae \
-#   --text_encoder_dir /home/jwhe/linyihan/lingbot-va-base/text_encoder \
-#   --tokenizer_dir /home/jwhe/linyihan/lingbot-va-base/tokenizer \
-#   --output_subdir latents_video_ft
+python scripts/robotwin_data_processing/extract_robotwin_video_latents.py \
+  --dataset_path /home/jwhe/linyihan/datasets/lerobot_robotwin_eef_clean_50 \
+  --vae_dir /home/jwhe/linyihan/lingbot-va-base/vae \
+  --text_encoder_dir /home/jwhe/linyihan/lingbot-va-base/text_encoder \
+  --tokenizer_dir /home/jwhe/linyihan/lingbot-va-base/tokenizer \
+  --output_subdir latents_video_ft
 
 # 2) Train video-only WAN finetune model (flow-matching velocity field)
-torchrun --nproc_per_node=8 wan_va/train.py --config robotwin_video_train
+# torchrun --nproc_per_node=8 wan_va/train.py --config robotwin_video_train
--- wan_va/configs/va_robotwin_train_cfg.py
+++ wan_va/configs/va_robotwin_train_cfg.py
@@ -8,9 +8,9 @@ va_robotwin_train_cfg.update(va_robotwin_cfg)
 
 # va_robotwin_train_cfg.resume_from = '/robby/share/Robotics/lilin1/code/Wan_VA_Release/train_out/checkpoints/checkpoint_step_10'
 
-va_robotwin_train_cfg.dataset_path = '/home/jwhe/linyihan/datasets/lerobot_robotwin_eef_clean_50'
+va_robotwin_train_cfg.dataset_path = '/home/jwhe/linyihan/datasets/lerobot_robotwin_eef_clean_50/adjust_bottle-demo_clean_collect_200-50'
 va_robotwin_train_cfg.empty_emb_path = os.path.join(va_robotwin_train_cfg.dataset_path, 'empty_emb.pt')
-va_robotwin_train_cfg.enable_wandb = True
+va_robotwin_train_cfg.enable_wandb = False
 va_robotwin_train_cfg.load_worker = 16
 va_robotwin_train_cfg.save_interval = 1000
 va_robotwin_train_cfg.gc_interval = 50
